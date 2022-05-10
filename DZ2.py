3b285d5a389c381c5723b1f258075a1250780431
UrbanovichO <urboleksander@gmail.com>
2022-05-10 09:36:05 +0300

    Dz2

---
+++ DZ2.py
@@ -0,0 +1,21 @@
+class Student:
+    print("Abobus")
+    a = 0
+    def __init__(self):
+        self.money = 100
+        self.study = 100
+#print("Ця гра на виживання. Вам потрібно прожити за студента 365 днів. Так щоб в вас були гроші (мінімум 100) і щоб вас не вигнали!!!")
+
+    def work(a, self=None):
+        for i in range(str(input("Скільки днів працювати студенту:"))):
+            self.money+10
+            self.study-10
+        for a in range(str(input("Скільки днів Вчитися студенту:"))):
+            self.money - 10
+            self.study + 10
+    if self.study < 1:
+        print("")
+    if self.money < 1:
+        print("")
+#student = Student()
+#student.money()
