53f6ffb963f9d1aeb51e6aa041f68ffc79789a94
UrbanovichO <urboleksander@gmail.com>
2022-05-11 16:14:56 +0300

    lesson5

--- Lesson5.py
+++ Lesson5.py
@@ -1,14 +1,41 @@
-class Class1:
-    var = 20
-    def __init__(self):
-        self.var = 10
-class Class2(Class1):
-    def __init__(self):
-        print(self.var)
-        super().__init__()
-        print(self.var)
-        print(super().var)
-obj = Class2()
+
+
+# class Computer:
+#     def __init__(self,model,*args,**kwargs):
+#         super().__init__(*args, **kwargs)
+#         self.model = model
+#         self.memory = 128
+#     def calculate(self):
+#         print("Calculating...")
+# class Display:
+#     def __init__(self,*args,**kwargs):
+#         super().__init__(*args,**kwargs)
+#         self.resolution = "4k"
+#     def display(self):
+#         print("I display the image on the screen...")
+# class Phone:
+#     def call(self):
+#         print("calling...")
+# class SmartPhon(Display,Computer,Phone):
+#     def printInfo(self):
+#         print(self.model)
+#         print(self.memory)
+#         print(self.resolution)
+# iphone = SmartPhon(model = "Last")
+# iphone.printInfo()
+
+
+# class Class1:
+#     var = 20
+#     def __init__(self):
+#         self.var = 10
+# class Class2(Class1):
+#     def __init__(self):
+#         print(self.var)
+#         super().__init__()
+#         print(self.var)
+#         print(super().var)
+# obj = Class2()
 
 
 # class Hello:
