d2fc153bfb07d35339d130b50e8e6f6ca0afaa36
UrbanovichO <urboleksander@gmail.com>
2022-05-16 16:20:14 +0300

    lesson 6 16.05

---
+++ lesson6.py
@@ -0,0 +1,33 @@
+# import requests
+#
+# def first_function():
+#     pass
+# class Human:
+#     pass
+# rq = requests
+# first_f  = first_function
+# nick = Human
+#
+# print(requests.__name__)
+# print(rq.__name__)
+# print(first_function.__name__)
+# print(first_f.__name__)
+# print(Human.__name__)
+# print(nick.__name__)
+# print(__name__)
+
+myList = []
+# for method in dir(myList):
+#     print(method)
+print(type(5))
+print(type(myList))
+print(type("hhh"))
+
+data = "string"
+print(hasattr(data,"reverse"))
+print(hasattr(data,"index"))
+
+
+print(getattr(data,"stratswitch"))
+print(getattr(data,"stratswitch",None))
+print(getattr(data,"reverse",None))
