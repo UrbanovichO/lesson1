db27f73e72ccc3e94d534158e61b5c0be756914d
UrbanovichO <urboleksander@gmail.com>
2022-05-18 15:55:32 +0300

    try - except

---
+++ lesson7.py
@@ -0,0 +1,34 @@
+# try:
+#     print("start program")
+#     print(hell)
+# except:
+#     print("You have an error")
+
+
+# res = 0
+# def division(a,b):
+#
+#     try:
+#         res = a/b
+#     except:
+#         try:
+#             a = int(a)
+#             b = int(b)
+#             res = a/b
+#         except ZeroDivisionError:
+#             res = 0
+#         except ValueError:
+#             res = ("Enter number")
+#     print(res)
+# while res == 0:
+#     division(input("a = "),input("b = "))
+
+# x = 15
+try:
+    print(x)
+except:
+    print("Something went wrong")
+else:
+    print("Else program do")
+finally:
+    print("The program has completed its work")
