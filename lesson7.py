20834b10bac619d296e61550fb3554d3d8f45623
UrbanovichO <urboleksander@gmail.com>
2022-05-18 16:13:37 +0300

    Create exception

--- lesson7.py
+++ lesson7.py
@@ -33,10 +33,23 @@
 # finally:
 #     print("The program has completed its work")
 
-def checker(var1):
-    if type(var1)!= str:
-        raise TypeError(f"Sorry, we can`t wark with {type(var1)}, we need class str")
-    else:
-        return var1
-fir = "10"
-checker(fir)
+# def checker(var1):
+#     if type(var1)!= str:
+#         raise TypeError(f"Sorry, we can`t wark with {type(var1)}, we need class str")
+#     else:
+#         return var1
+# fir = "10"
+# checker(fir)
+
+class BuildingError(Exception):
+    def __str__(self):
+        return f"With so much material the house cannot be built!"
+def check_material(amount_of_material, limit_value):
+        if amount_of_material > limit_value:
+            return "enough material"
+        else:
+            raise BuildingError(amount_of_material)
+
+materials = 500
+check_material(materials,300)
+
