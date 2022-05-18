64022448d318fb50441bf7a44bc02ac696d49609
UrbanovichO <urboleksander@gmail.com>
2022-05-18 16:21:34 +0300

    Warning

--- lesson7.py
+++ lesson7.py
@@ -41,15 +41,22 @@
 # fir = "10"
 # checker(fir)
 
-class BuildingError(Exception):
-    def __str__(self):
-        return f"With so much material the house cannot be built!"
-def check_material(amount_of_material, limit_value):
-        if amount_of_material > limit_value:
-            return "enough material"
-        else:
-            raise BuildingError(amount_of_material)
+# class BuildingError(Exception):
+#     def __str__(self):
+#         return f"With so much material the house cannot be built!"
+# def check_material(amount_of_material, limit_value):
+#         if amount_of_material > limit_value:
+#             return "enough material"
+#         else:
+#             raise BuildingError(amount_of_material)
+#
+# materials = 500
+# check_material(materials,300)
 
-materials = 500
-check_material(materials,300)
 
+import warnings
+warnings.simplefilter("ignore",SyntaxWarning)
+warnings.simplefilter("always",ImportWarning)
+
+# warnings.warn("Warning, no code here", SyntaxWarning)
+warnings.warn("Warning, module not import", ImportWarning)
