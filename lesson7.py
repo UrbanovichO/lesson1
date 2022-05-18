8b106d184647ab2159b9685768f07dd6bc5983d6
UrbanovichO <urboleksander@gmail.com>
2022-05-18 16:01:56 +0300

    raise

--- lesson7.py
+++ lesson7.py
@@ -24,11 +24,19 @@
 #     division(input("a = "),input("b = "))
 
 # x = 15
-try:
-    print(x)
-except:
-    print("Something went wrong")
-else:
-    print("Else program do")
-finally:
-    print("The program has completed its work")
+# try:
+#     print(x)
+# except:
+#     print("Something went wrong")
+# else:
+#     print("Else program do")
+# finally:
+#     print("The program has completed its work")
+
+def checker(var1):
+    if type(var1)!= str:
+        raise TypeError(f"Sorry, we can`t wark with {type(var1)}, we need class str")
+    else:
+        return var1
+fir = "10"
+checker(fir)
