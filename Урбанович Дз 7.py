b041be9dcba88ba383ec3aed2e9162ac6e8ac968
UrbanovichO <urboleksander@gmail.com>
2022-05-21 17:41:47 +0300

    Дз 6 (1)

---
+++ Урбанович Дз 7.py
@@ -0,0 +1,13 @@
+result = []
+kem = str(150)
+def divider(a, b):
+    if a < b:
+        raise ValueError
+    if b > 100:
+        raise IndexError
+    return a/b
+data = {10: 2, 2: 5, "123": 4, 18: 0, tuple([]): 15, 8 : 4}
+for key in data:
+    res = divider(key, data[kem])
+    result.append(res)
+print(result)
