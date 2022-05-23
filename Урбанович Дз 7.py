dba558de387b7c6a1aaa92ab3c6bab508ccfa9bf
UrbanovichO <urboleksander@gmail.com>
2022-05-23 15:27:39 +0300

    Дз 6 (4)

--- Урбанович Дз 7.py
+++ Урбанович Дз 7.py
@@ -7,8 +7,9 @@ def divider(a, b):
     return a/b
 
 #data = {10: 2, 2: 5, "123": 4, 18: 0, []: 15, 8 : 4}
-data = {10: 2, 2: 5, "123": 4, 18: 0 , 8 : 4}
-data["0"] = 15
+data = {10: 2, 2: 5, 18: 0 , 8 : 4}
+data[0] = 15
+data[123] = 4
 for key in data:
     try:
         res = divider(key, data[key])
