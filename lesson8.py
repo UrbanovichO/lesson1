79d4d01f8cee1e4882aa234d59bf0de0e72745aa
UrbanovichO <urboleksander@gmail.com>
2022-05-23 16:30:13 +0300

    lesson 8 generations

--- lesson8.py
+++ lesson8.py
@@ -5,19 +5,31 @@
 #
 # for i in myList:
 #     print(next(it)+2)
-
-class Counter:
-    def __init__(self,maxNum):
-        self.i = 0
-        self.maxNumber = maxNum
-    def __iter__(self):
-        self.i = 0
-        return self
-    def __next__(self):
-        self.i += 1
-        if self.i > self.maxNumber:
-            raise StopIteration
-        return self.i
-count = Counter(5)
-for elem in count:
-    print(elem)
+#
+# class Counter:
+#     def __init__(self,maxNum):
+#         self.i = 0
+#         self.maxNumber = maxNum
+#     def __iter__(self):
+#         self.i = 0
+#         return self
+#     def __next__(self):
+#         self.i += 1
+#         if self.i > self.maxNumber:
+#             raise StopIteration
+#         return self.i
+# count = Counter(5)
+p = range(0,5)
+it = iter(p)
+a = [(1+next(it)*5) for i in range(5)]
+print(a)
+# for elem in count:
+#     print(elem)
+# import random
+#
+# l = []
+# for i in range(0,10):
+#     l.append(random.randint(-10,10))
+# print(l)
+# a = [random.randint(-10,10)//3 for i in range(10)]
+# print(a)
