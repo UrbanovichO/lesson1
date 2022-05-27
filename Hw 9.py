4944fcf296714734fc129ac7383f223fe4a85c51
UrbanovichO <urboleksander@gmail.com>
2022-05-27 09:01:16 +0300

    Hw 9

---
+++ Hw 9.py
@@ -0,0 +1,12 @@
+import random
+import logging
+logging.basicConfig(level=logging.DEBUG, filename="logs.log",filemode="w",format="We have next logging massege:%(asctime)s%(levelname)s - %(message)s")
+a1 = random.randint(1,10)
+r =range(0,10)
+it = iter(r)
+l = [a1+5*(next(it))/random.randint(-5, 5) for i in r]
+it = iter(l)
+try:
+    print(l)
+except:
+    logging.error("Division Zero")
