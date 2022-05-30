01bb29ce23480687e4f19145832068ac3ce320e8
UrbanovichO <urboleksander@gmail.com>
2022-05-30 15:47:13 +0300

    lesson 10 import requests

--- lesson10.py
+++ lesson10.py
@@ -1,5 +1,3 @@
-import urllib.request
-opener = urllib.request.build_opener()
-response = opener.open("https://httpbin.org/get")
-print(response.read())
-
+import requests
+response = requests.get("https://httpbin.org/get")
+print(response.content)
