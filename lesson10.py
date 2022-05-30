db75b82e3662f5ebbd075c1da4c77b9acdb9244d
UrbanovichO <urboleksander@gmail.com>
2022-05-30 16:18:08 +0300

    lesson 10 parser

--- lesson10.py
+++ lesson10.py
@@ -3,6 +3,17 @@ import requests
 # print(response.text)
 # print(f"Datatype - {type(response.text)}")
 
-response = requests.post("https://httpbin.org/post",data="test data",
-                         headers={"h1":"test title"})
-print(response.text)
+# response = requests.post("https://httpbin.org/post",data="test data",headers={"h1":"test title"})
+# print(response.text)
+res_parse_list = []
+response = requests.get("https://coinmarketcap.com/")
+response_text = response.text
+response_parse = response_text.split("<span>")
+for parse_elem_1 in response_parse:
+    if parse_elem_1.startswith("$"):
+        for parse_elem_2 in parse_elem_1.split("</span>"):
+            if parse_elem_2.startswith("$") and parse_elem_2[1].isdigit():
+                res_parse_list.append(parse_elem_2)
+
+        bitcoin_exchange_rate = res_parse_list[0]
+        print(bitcoin_exchange_rate)
