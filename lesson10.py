7a1f5ec49dc244a151011c4d28cea4fbbcc958c9
UrbanovichO <urboleksander@gmail.com>
2022-05-30 15:58:24 +0300

    lesson 10 requests.post

--- lesson10.py
+++ lesson10.py
@@ -1,4 +1,8 @@
 import requests
-response = requests.get("https://httpbin.org/get")
-print(response.content)
-print(f"Datatype - {type(response.content)}")
+# response = requests.get("https://httpbin.org/get")
+# print(response.text)
+# print(f"Datatype - {type(response.text)}")
+
+response = requests.post("https://httpbin.org/post",data="test data",
+                         headers={"h1":"test title"})
+print(response.text)
