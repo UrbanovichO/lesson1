4b1de31e09cadb91637b1351e2ba31535115c9df
UrbanovichO <urboleksander@gmail.com>
2022-06-01 16:06:33 +0300

    lesson 11 difference

--- lesson 11.py
+++ lesson 11.py
@@ -1,16 +1,22 @@
+# Problem 1
 # a = input("Input number a:")
 # b = input("Input number b:")
 # c = int(a)*int(b)//2
 # print("%.2f" % c)
-
+# Problem 2
 # a = float(input("Input number:"))
 # b = float(input("Input number:"))
 # c = float(input("Input number:"))
 # d = float(input("Input number:"))
 # sum = a+b+c+d
 # print("%.4f" % sum)
-
-a = int(input("Input number:"))
-b = int(input("Input number:"))
-P = (a*2)+(b*2)
-print(P)
+# Problem 3
+# a = int(input("Input number:"))
+# b = int(input("Input number:"))
+# P = (a*2)+(b*2)
+# print(P)
+# Problem 4
+x = int(input("Input number:"))
+y = int(input("Input number:"))
+difference = x-y
+print(difference)
