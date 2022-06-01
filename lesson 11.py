327bf171dfa2ce0d9af2662414f5a23e9cad9d9d
UrbanovichO <urboleksander@gmail.com>
2022-06-01 15:55:51 +0300

    lesson 11 Sum

--- lesson 11.py
+++ lesson 11.py
@@ -1,5 +1,11 @@
-a = input("Input number a:")
-b = input("Input number b:")
-c = int(a)*int(b)//2
-print("%.2f" % c)
+# a = input("Input number a:")
+# b = input("Input number b:")
+# c = int(a)*int(b)//2
+# print("%.2f" % c)
 
+a = float(input("Input number:"))
+b = float(input("Input number:"))
+c = float(input("Input number:"))
+d = float(input("Input number:"))
+sum = a+b+c+d
+print("%.4f" % sum)
