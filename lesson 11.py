aaa52de27279bc86b5e0116a882206aa251b595a
UrbanovichO <urboleksander@gmail.com>
2022-06-04 20:23:29 +0300

    Hw 11

--- lesson 11.py
+++ lesson 11.py
@@ -1,26 +1,26 @@
-# Problem 1
+# Problem 1000
 # a = input("Input number a:")
 # b = input("Input number b:")
 # c = int(a)*int(b)//2
 # print("%.2f" % c)
-# Problem 2
+# Problem 1001
 # a = float(input("Input number:"))
 # b = float(input("Input number:"))
 # c = float(input("Input number:"))
 # d = float(input("Input number:"))
 # sum = a+b+c+d
 # print("%.4f" % sum)
-# Problem 3
+# Problem 1002
 # a = int(input("Input number:"))
 # b = int(input("Input number:"))
 # P = (a*2)+(b*2)
 # print(P)
-# Problem 4
+# Problem 1006
 # x = int(input("Input number:"))
 # y = int(input("Input number:"))
 # difference = x-y
 # print(difference)
-# Problem 5
-N = float(input("Input number:"))
-a = int(N)*2
-print(a)
+# Problem 1007
+# N = float(input("Input number:"))
+# a = int(N)*2
+# print(a)
